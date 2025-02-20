5dd6eb74349b25d3963b70a98d9dde0ee33e880b
thanbwe <khunthanbwe1998@gmail.com>
2025-02-20 22:10:50 +0630

    js lesson

---
+++ BATCH4JS/custom.js
@@ -0,0 +1,27 @@
+var name = "Kyaw Zin Aung";
+        document.write(name);
+        document.write('<br>');
+
+        var name = "Kyaw Zin Aung Update";
+        document.write(name);
+        document.write('<br>');
+
+
+
+        let fatherName = "U Kyaw San Oo";
+        document.write(fatherName);
+        document.write('<br>');
+
+        fatherName = "U Kyaw San Oo Update";
+        document.write(fatherName);
+        document.write('<br>');
+
+
+
+        const motherName = "Daw Khin Ohn";
+        document.write(motherName);
+        document.write('<br>');
+
+        motherName = "Daw Khin Ohn Update";
+        document.write(motherName);
+        document.write('<br>');
